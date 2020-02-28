db7a9a417cf8e8b55810e7d375445e87d9d4615d
nonatorw <wellingtonrodrigo.nonato@gmail.com>
2020-02-27 23:00:20 -0300

    Add Product List

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from "@angular/core";
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
 
 @Component({
   selector: "app-product-alerts",
@@ -6,8 +6,8 @@ import { Component, OnInit, Input } from "@angular/core";
   styleUrls: ["./product-alerts.component.css"]
 })
 export class ProductAlertsComponent implements OnInit {
-
   @Input() product;
+  @Output() notify = new EventEmitter();
 
   constructor() {}
 
